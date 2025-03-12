8331cba0b13da4c308f2275f23180696b518e079
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-12 18:09:24 -0300

    feat: implement form with username input and submit button

--- exercises/07.forms/01.problem.form/index.tsx
+++ exercises/07.forms/01.problem.form/index.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 
 function App() {
-	// 🐨 render a form
-	//   🐨 render a "Username" label
-	//   🐨 render an input with the name "username"
-	//   🐨 render a button
-	// 💯 associate the label to the input using htmlFor and id attributes
-	// 💯 explicitly set the button type appropriately
-	return 'TODO'
+	return (
+		<form>
+			<label htmlFor="usernameInput">Username</label>
+			<input id="usernameInput" name="username" type="text" />
+			<button type="submit">submit</button>
+		</form>
+	)
 }
 
 const rootEl = document.createElement('div')
