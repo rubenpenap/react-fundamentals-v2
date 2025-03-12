4ab61d9f10aedf8eea591589ddf2f1c153b1ba4c
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-12 00:45:47 -0300

    refactor: add size prop to Box component for responsive styling

--- exercises/06.styling/03.problem.size-prop/index.tsx
+++ exercises/06.styling/03.problem.size-prop/index.tsx
@@ -1,22 +1,19 @@
 import { createRoot } from 'react-dom/client'
 
 export function Box({
-	// 💯 you can keep the style and className props here, but you can make this
-	// still work if you remove them. Give that a shot if you want.
-	style = {},
-	className = '',
-	// 🐨 add a size prop here
-	...otherProps // 🦺 intersect (&) this with an object that has a size prop type here which is
-	// optional and is one of "small", "medium", or "large"
-}: React.ComponentProps<'div'>) {
-	// 🐨 based on the size prop, define a new variable called sizeClassName
+	size,
+	...otherProps
+}: {
+	size?: 'small' | 'medium' | 'large'
+} & React.ComponentProps<'div'>) {
+	const sizeClassName = size ? `box--${size}` : ''
 	return (
 		<div
-			// 🐨 add the sizeClassName to the className prop
-			// 💯 for something extra, handle the case where no className is given (remove extra spaces).
-			className={`box ${className}`}
-			style={{ fontStyle: 'italic', ...style }}
 			{...otherProps}
+			className={['box', otherProps.className, sizeClassName]
+				.filter(Boolean)
+				.join(' ')}
+			style={{ fontStyle: 'italic', ...otherProps.style }}
 		/>
 	)
 }
@@ -24,14 +21,13 @@ export function Box({
 function App() {
 	return (
 		<div>
-			{/* 🐨 update all these boxes to use the size prop */}
-			<Box className="box--small" style={{ backgroundColor: 'lightblue' }}>
+			<Box size="small" style={{ backgroundColor: 'lightblue' }}>
 				small lightblue box
 			</Box>
-			<Box className="box--medium" style={{ backgroundColor: 'pink' }}>
+			<Box size="medium" style={{ backgroundColor: 'pink' }}>
 				medium pink box
 			</Box>
-			<Box className="box--large" style={{ backgroundColor: 'orange' }}>
+			<Box size="large" style={{ backgroundColor: 'orange' }}>
 				large orange box
 			</Box>
 			<Box>sizeless colorless box</Box>
