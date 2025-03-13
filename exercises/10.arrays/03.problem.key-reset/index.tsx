d848d375c953e4b1fae13fc3e67d505ffa0b7f7a
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-12 22:51:10 -0300

    feat: add key prop to input for dynamic resetting in App component

--- exercises/10.arrays/03.problem.key-reset/index.tsx
+++ exercises/10.arrays/03.problem.key-reset/index.tsx
@@ -5,9 +5,8 @@ function App() {
 	const [key, setKey] = useState(0)
 	return (
 		<div>
-			{/* 🐨 add a key prop to this input and set it to the key state */}
-			<input />
-			<button onClick={() => setKey(key => key + 1)}>Reset</button>
+			<input key={key} />
+			<button onClick={() => setKey((key) => key + 1)}>Reset</button>
 		</div>
 	)
 }
